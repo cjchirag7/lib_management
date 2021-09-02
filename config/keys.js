8be2b3eec3b862899f272363012e76a38fc9da9f
Chirag Jain <cjchirag7@gmail.com>
2021-09-02 13:36:38 +0530

    Updated npm packages

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    mongoURI: 'mongodb+srv://admin:pass123admin@cluster0-fclkr.mongodb.net/test?retryWrites=true&w=majority',
-    secretKey: '19945-62890-06876-52361'
+    mongoURI: process.env.MONGO_URI,
+    secretKey: process.env.SECRET_KEY
 }
